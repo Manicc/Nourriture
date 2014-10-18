0a16a0e6aaa3403bc9259fc56c7b3d44fd7a7530
Manic <329541945@qq.com>
2014-10-19 00:59:42 +0800

    add models //not syncdb yet

--- source/server/common/models.py
+++ source/server/common/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    grade = models.IntegerField(default = 0)
+    grad_count = models.IntegerField(default=0)
+    count = models.IntegerField()
+    suppliers = models.ManyToManyField(Supplier)
+
+
+class Supplier(models.Model):
+    grade = models.IntegerField(default = 0)
+    grad_count = models.IntegerField(default=0)
+    commentId = models.IntegerField()
+    foodId = models.IntegerField()
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User)
+    desc = models.CharField(max_length=100)
+    process = models.TextField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    comment_type = models.CharField(max_length=50)
+
+
+class Collect(models.Model):
+    userID = models.ForeignKey(User)
+    collect_type = models.CharField(max_length=50)
+
+
+class FoodUserMap(models.Model):
+    userID = models.ForeignKey(User)
+    foodId = models.ForeignKey(Food)
