37301bfcf312e107d9c1feaf9a44b2fd492d7439
Manic <329541945@qq.com>
2014-10-19 10:35:24 +0800

    add admin.py

---
+++ source/server/common/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from common.models import Food, Recipe, Comment, Collect
+
+admin.site.register(Food)
+admin.site.register(Recipe)
+admin.site.register(Comment)
+admin.site.register(Collect)
