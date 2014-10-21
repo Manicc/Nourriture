844964d84471f7877f8ac2f281e6947b0869a1b6
linan <417079463@qq.com>
2014-10-21 20:52:37 +0800

    recipe url/view 修改

--- source/server/recipe/urls.py
+++ source/server/recipe/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+    url(r'^$', 'recipe.views._list'),
+    url(r'^(?P<id>\d+)$', 'recipe.views.detail'),
 )
--- source/server/recipe/views.py
+++ source/server/recipe/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.core import serializers
+from django.http import HttpResponse
+from common.models import Recipe
+
+def _list(request):
+    recipes = Recipe.objects.all()
+    data = serializers.serialize("json", recipes)
+    response_kwargs = dict()
+    response_kwargs['content_type'] = 'application/json'
+    return HttpResponse(data, **response_kwargs)
+
+
+def detail(request, id):
+    recipe = Recipe.objects.get(id=id)
+    data = serializers.serialize("json", [recipe])
+    response_kwargs = dict()
+    response_kwargs['content_type'] = 'application/json'
+    return HttpResponse(data, **response_kwargs)
