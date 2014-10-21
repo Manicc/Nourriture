66dd4d6c0c7a8a9b088a63d5d6de8c01efb76ca1
tsplaycool <916759791@qq.com>
2014-10-21 14:31:06 +0800

    获取所有product 数据以及根据id获取product数据

--- source/server/product/urls.py
+++ source/server/product/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+    url(r'^$', 'product.views._list'),
+    url(r'^(?P<id>\d+)$', 'product.views.detail'),
 )
--- source/server/product/views.py
+++ source/server/product/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.core import serializers
+from django.http import HttpResponse
+from common.models import Product
+
+
+def _list(request):
+    products = Product.objects.all()
+    data = serializers.serialize("json", products)
+    response_kwargs = dict()
+    response_kwargs['content_type'] = 'application/json'
+    return HttpResponse(data, **response_kwargs)
+
+
+def detail(request, id):
+    product = Product.objects.get(id=id)
+    data = serializers.serialize("json", [product])
+    response_kwargs = dict()
+    response_kwargs['content_type'] = 'application/json'
+    return HttpResponse(data, **response_kwargs)
