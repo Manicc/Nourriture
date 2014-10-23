fc9e358e7f84168621b3d5445af083e28475c945
liao jiaming <ljmvty@163.com>
2014-10-23 23:42:28 +0800

    new add files

---
+++ source/server/gastronomist/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'LJM'
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'gastronomist.views._list'),
+    url(r'^(?P<id>\d+)$', 'gastronomist.views.detail'),
+)
