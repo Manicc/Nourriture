da4bbe0aa58bfb84f3023a82874031416e762e9c
linan <417079463@qq.com>
2014-10-23 12:05:13 +0800

    recipe search

--- source/server/recipe/urls.py
+++ source/server/recipe/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'recipe.views._list'),
     url(r'^(?P<id>\d+)$', 'recipe.views.detail'),
+    url(r'searchrecipe/$','recipe.views.search'),
 )
--- source/server/recipe/views.py
+++ source/server/recipe/views.py
@@ -1,6 +1,8 @@
 from django.core import serializers
 from django.http import HttpResponse
 from common.models import Recipe
+from common.models import Ingredient
+from common.models import User
 
 def _list(request):
     recipes = Recipe.objects.all()
@@ -13,6 +15,29 @@ def _list(request):
 def detail(request, id):
     recipe = Recipe.objects.get(id=id)
     data = serializers.serialize("json", [recipe])
+    response_kwargs = dict()
+    response_kwargs['content_type'] = 'application/json'
+    return HttpResponse(data, **response_kwargs)
+
+def search(request):
+    """
+        search: using ingredient name and gastronomist name
+        form: http://127.0.0.1:8000/recipe/searchrecipe?gastronomistname=linan&ingredientname=1
+    """
+    ingredientname = request.GET.get('ingredientname')
+    gastronomistname = request.GET.get('gastronomistname')
+
+    ingredient = Ingredient.objects.get(name=ingredientname)
+    ingredientid = ingredient.id
+    gastronomist = User.objects.get(username=gastronomistname)
+    gastronomistid = gastronomist.id
+
+    if(gastronomistid!=''):
+        recipes = Recipe.objects.filter(gastronomist=ingredientid)
+    if(ingredientid!=''):
+        recipes = recipes.filter(ingredients=ingredientid)
+
+    data = serializers.serialize("json", recipes)
     response_kwargs = dict()
     response_kwargs['content_type'] = 'application/json'
     return HttpResponse(data, **response_kwargs)
