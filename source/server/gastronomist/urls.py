10be1b47367d3f34ae089555a4f7da2658bea6e8
liao jiaming <ljmvty@163.com>
2014-10-31 23:23:20 +0800

    search Gastronomist use name

--- source/server/gastronomist/urls.py
+++ source/server/gastronomist/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'gastronomist.views._list'),
     url(r'^(?P<id>\d+)$', 'gastronomist.views.detail'),
-    url(r'gastronomist/$','gastronomist.views.search'),
+    url(r'search/$','gastronomist.views.search'),
 )
