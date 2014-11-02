a39afbcf61cb47df19b949c0c8cffa6601f76210
tanshuai <916759791@qq.com>
2014-11-02 20:00:12 +0800

    product表 支持name和ingredient查询

--- source/server/product/views.py
+++ source/server/product/views.py
@@ -22,8 +22,22 @@ def detail(request, id):
 
 
 def search(request):
+    name_param = request.GET.get('name', '')
+    ingredient_param = request.GET.get('ingredient', '')
     tag_param = request.GET.get('tag', '')
-    if tag_param != '':
+    if name_param != '':
+        name_products = Product.objects.filter(name__contains=name_param)
+        data = serializers.serialize("json", name_products)
+    elif ingredient_param != '':
+        ingredient_param_list = ingredient_param.split(',')
+        result = []
+        products = Product.objects.all()
+        for p in products:
+            for ingredient_param_list_name in ingredient_param_list:
+                p.ingredients.filter(name__contains=ingredient_param_list_name)
+            result.insert(-1, p)
+        data = serializers.serialize("json", result)
+    elif tag_param != '':
         tag_param_list = tag_param.split(',')
         for tag_param_list_name in tag_param_list:
             product_tags = ProductTag.objects.filter(name=tag_param_list_name)
@@ -39,6 +53,7 @@ def search(request):
             products = product_tag.product.all();
 
         data = serializers.serialize("json", products)
-        response_kwargs = {'content_type': 'application/json'}
-        return HttpResponse(data, **response_kwargs)
+
+    response_kwargs = {'content_type': 'application/json'}
+    return HttpResponse(data, **response_kwargs)
 
