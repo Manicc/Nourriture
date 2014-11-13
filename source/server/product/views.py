2b8f1bd3c3c3a0c03893e517dd6cfa28d387beb3
Manic <329541945@qq.com>
2014-11-13 19:06:08 +0800

    update product search

--- source/server/product/views.py
+++ source/server/product/views.py
@@ -24,23 +24,30 @@ def search(request):
     name_param = request.GET.get('name', '')
     ingredient_param = request.GET.get('ingredient', '')
     tag_param = request.GET.get('tag', '')
+    products = []
 
     query = Q()
 
     if name_param:
         query = Q(name__contains=name_param)
 
-    # TODO not implement
-    if ingredient_param:
-        ingredient_param_list = ingredient_param.split(',')
-        temp_query = Q(ingredients__name__in = ingredient_param_list)
-
-        query = query & temp_query
-
     if tag_param:
         query = query & Q(tags__name=tag_param)
 
-    products = Product.objects.filter(query)
+    if query.children:
+        products = Product.objects.filter(query)
+
+    if ingredient_param:
+        ingredient_param_list = ingredient_param.split(',')
+        if not products:
+            products = Product.objects.filter(ingredients__name__contains = ingredient_param_list[0])
+        else:
+            products = products.filter(ingredients__name__contains = ingredient_param_list[0])
+
+        for name in ingredient_param_list[1:]:
+            if name:
+                products = products.filter(ingredients__name__contains = name)
+
     data = serializers.serialize("json", products)
 
     response_kwargs = {'content_type': 'application/json'}
