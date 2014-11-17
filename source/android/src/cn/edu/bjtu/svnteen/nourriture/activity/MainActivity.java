fe32405e974392229b428c70adc6b57c7b973941
shuai.tan <916759791@qq.com>
2014-11-17 14:34:38 +0800

    删除一些注释

--- source/android/src/cn/edu/bjtu/svnteen/nourriture/activity/MainActivity.java
+++ source/android/src/cn/edu/bjtu/svnteen/nourriture/activity/MainActivity.java
@@ -32,39 +32,7 @@ public class MainActivity extends FragmentActivity implements IAppObserver {
 		init();
 
 		MessageManager.getInstance()
-				.attachMessage(MessageID.OBSERVER_APP, this);
-		// JumperUtils.JumpToKtv();
-		// StThreadPool.runThread(JobType.NET, new Runnable() {
-		//
-		// @Override
-		// public void run() {
-		// HttpClient client = new DefaultHttpClient();
-		// HttpGet method = new HttpGet(
-		// "http://www.bjtu.edu.cn/images/siyuanblue/fla.jpg");
-		// try {
-		// HttpResponse response = client.execute(method);
-		// HttpEntity entity = response.getEntity();
-		// byte[] bs = EntityUtils.toByteArray(entity);
-		// final Bitmap b = BitmapFactory.decodeByteArray(bs, 0,
-		// bs.length);
-		// MessageManager.getInstance().asyncRun(new Runner() {
-		//
-		// @Override
-		// public void call() {
-		// mImageView.setImageBitmap(b);
-		// Intent intent = new Intent(MainActivity.this,
-		// Activity2.class);
-		// // startActivity(intent);
-		// JumperUtils.JumpToKtv2();
-		// }
-		// });
-		// } catch (Exception e) {
-		// String a = e.toString();
-		// System.out.println(a);
-		// }
-		// }
-		// });
-		// http://www.bjtu.edu.cn/images/siyuanblue/fla.jpg
+				.attachMessage(MessageID.OBSERVER_APP, this);// 例子：怎么使用attachMessage方法
 	}
 
 	// 一些需要初始化的类和方法
