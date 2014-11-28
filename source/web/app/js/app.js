3ce369dd6976ab7482cdcc7e340e849bca50b301
Manic <329541945@qq.com>
2014-11-28 21:28:08 +0800

    add detial views of ingredient

--- source/web/app/js/app.js
+++ source/web/app/js/app.js
@@ -10,13 +10,25 @@ app.config(['$routeProvider','$locationProvider',
         templateUrl: 'view/root.html',
         controller: 'RootCtrl'
       }).
-      when('/ingredient', {
-        templateUrl: 'view/ingredient_list.html',
+      when('/ingredient/', {
+        templateUrl: 'view/ingredients.html',
         controller: 'IngredientListCtrl'
+      }).
+      when('/ingredient/:id', {
+        templateUrl: 'view/ingredient_detial.html',
+        controller: 'IngredientDetialCtrl'
+      }).
+      when('/product/', {
+        templateUrl: 'view/products.html',
+        controller: 'ProductListCtrl'
+      }).
+      when('/recipe/', {
+        templateUrl: 'view/recipes.html',
+        controller: 'RecipeListCtrl'
       }).
       otherwise({
         redirectTo: '/'
       });
 
-      $locationProvider.html5Mode(true);
+      //$locationProvider.html5Mode(true);
   }]);
--- source/web/app/js/controllers.js
+++ source/web/app/js/controllers.js
@@ -1,8 +1,34 @@
 var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('RootCtrl', function ($scope, $http) {
+  /*$http.get('http://nourriture.sinaapp.com/ingredient/').success(function(data){
+  	$scope.ingredients = data;
+  })*/
+
+});
+
+appControllers.controller('IngredientListCtrl', function ($scope, $http) {
   $http.get('http://nourriture.sinaapp.com/ingredient/').success(function(data){
   	$scope.ingredients = data;
+  })
+});
+
+appControllers.controller('IngredientDetialCtrl', function ($scope, $http, $routeParams) {
+  $http.get('http://nourriture.sinaapp.com/ingredient/'+ $routeParams.id).success(function(data){
+  	$scope.ingredient = data[0];
+  })
+});
+
+appControllers.controller('ProductListCtrl', function ($scope, $http) {
+  $http.get('http://nourriture.sinaapp.com/product/').success(function(data){
+  	$scope.products = data;
+  })
+
+});
+
+appControllers.controller('RecipeListCtrl', function ($scope, $http) {
+  $http.get('http://nourriture.sinaapp.com/recipe/').success(function(data){
+  	$scope.recipes = data;
   })
 
 });
