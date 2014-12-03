5cef7e00768974fbb6addb4635f05d4aa4bd201d
Manic <329541945@qq.com>
2014-12-04 01:18:04 +0800

    success upgrade to django 1.7.1

--- source/server/server/wsgi.py
+++ source/server/server/wsgi.py
@@ -13,7 +13,7 @@ import sys
 root = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test123.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
