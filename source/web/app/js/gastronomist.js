e077d8bc52d2674d006b4637128d21092a00d8dd
liao jiaming <ljmvty@163.com>
2014-12-11 21:17:56 +0800

    gastronomist

--- source/web/app/js/gastronomist.js
+++ source/web/app/js/gastronomist.js
@@ -1,5 +1,12 @@
-var gastronomist = angular.module('gastronomist', ['config'])
+var gastronomistAPP = angular.module('gastronomistAPP', []);
 
-//url configuration
-
-//controllers
+gastronomistAPP.controller('FoodListCtrl', function ($scope) {
+  $scope.foods = [
+    {"name": "cooked rice",
+     "snippet": "made with good rice."},
+    {"name": "dumplings",
+     "snippet": "boiled pork dumpling"},
+    {"name": "apple",
+     "snippet": "apple can't not eat."}
+  ];
+});
